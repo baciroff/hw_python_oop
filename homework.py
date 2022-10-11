56cf4c6bf04700b6ab124e7ea7bdb84bc758c61c
Руслан Бакиров <baciroff82@gmail.com>
2022-10-11 23:47:49 +0300

    corrected code according to comments

--- homework.py
+++ homework.py
@@ -84,8 +84,8 @@ class SportsWalking(Training):
         weight = self.weight
         height = self.height
         calorie = weight * ((self.COEF_1 + (speed ** self.
-                             COEF_2 // height) * self.COEF_3)) * (self.
-                              duration * self.MIN_IN_HOUR)
+                  COEF_2 // height) * self.COEF_3)) * (self.
+                  duration * self.MIN_IN_HOUR)
         return calorie
 
 
